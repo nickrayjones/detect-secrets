243b0272bb115b91000e845dba4c9fb9aaaa4492
Stephen Linnell <91089808+sclinnell@users.noreply.github.com>
2025-10-02 14:59:49 -0400

    Update setup.py
    Signed-off-by: Stephen Linnell <91089808+sclinnell@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'pyyaml',
         'requests',
-        'urllib3<2.0.0',
+        'urllib3>2.5.0',
         'boxsdk[jwt]<4.0.0',
         'packaging',
         'tabulate',
